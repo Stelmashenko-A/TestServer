e2b4443ad795b6a69af876d3c54389dbae0b4acf
Stelmashenko-A <stelma_28@bk.ru>
2015-09-18 18:51:21 +0300

    Interface creation

---
+++ TestServer/TestServer/ITester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TestServer
+{
+    internal interface ITester
+    {
+        void Test(Process proc, IList<string> testCases, string inputDirectory, string outputDirectory, int failedTest,
+            FailReason reason);
+    }
+}
